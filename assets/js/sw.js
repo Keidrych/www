6e34b97e18115dd0dc9fa03faa802f1dd1414a16
keidrych@tayloredtechnology.net <keidrych@tayloredtechnology.net>
2019-07-16 16:09:16 +0000

    Deploy from Forestry.io

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -28,7 +28,7 @@ const SHELL_CACHE = "shell-8.4.0--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2019-07-16T12:26:46+00:00--sw/";
+const CONTENT_CACHE = "content--2019-07-16T16:09:10+00:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const CACHE_SEARCH_PARAM = "sw-cache";
@@ -58,8 +58,8 @@ const SHELL_FILES = [
 ];
 
 const ASSET_FILES = [
-  /**/ "/assets/img/sidebar-bg.jpg" /**/,
-  /**/ "/assets/icons/icon.png" /**/,
+  /**/ "https://res.cloudinary.com/tayloredtechnology/image/upload/c_scale,w_auto:100,dpr_auto/v1563282615/keidrych.com/bg.jpg" /**/,
+  /**/ "https://res.cloudinary.com/tayloredtechnology/image/upload/v1563282614/keidrych.com/keidrych.jpg" /**/,
   /**/
 ];
 
--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts("/assets/js/sw.js?t=2019-07-16T12:26:46+00:00");
+importScripts("/assets/js/sw.js?t=2019-07-16T16:09:10+00:00");
