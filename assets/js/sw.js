c5460c84e1f6421df3abeda439ae143de8a4bb80
keidrych@tayloredtechnology.net <keidrych@tayloredtechnology.net>
2019-07-16 16:51:41 +0000

    Deploy from Forestry.io

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -28,7 +28,7 @@ const SHELL_CACHE = "shell-8.4.0--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2019-07-16T16:34:42+00:00--sw/";
+const CONTENT_CACHE = "content--2019-07-16T16:51:36+00:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const CACHE_SEARCH_PARAM = "sw-cache";
--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts("/assets/js/sw.js?t=2019-07-16T16:34:42+00:00");
+importScripts("/assets/js/sw.js?t=2019-07-16T16:51:36+00:00");
